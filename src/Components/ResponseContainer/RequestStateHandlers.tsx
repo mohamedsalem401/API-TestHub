f65527acd0bf808a6c4dfe6cc42b2fe8aa9f75bc
mohamed yahia <mohamedsalem401@yahoo.com>
2024-01-28 03:52:34 +0200

    Removed request state handler

--- src/Components/ResponseContainer/RequestStateHandlers.tsx
+++ src/Components/ResponseContainer/RequestStateHandlers.tsx
@@ -1,42 +1,17 @@
-import { HttpState } from '../../state/types';
-import { AxiosResponse } from 'axios';
+export function getResponseExtension() {
+  return (headers: Record<string, string>) => {
+    const contentType = headers['content-type'];
 
-export function getResonseBody(index: number): AxiosResponse<any, any>['data'] | undefined {
-  return (state: HttpState[]) => {
-    return state[index].response.responseObject?.data;
-  };
-}
-export function getCode(index: number): (state: HttpState[]) => number | undefined {
-  return (state: HttpState[]) => {
-    return state[index].response.responseObject?.status || 200;
-  };
-}
-export function getStatus(index: number) {
-  return (state: HttpState[]) => {
-    return state[index].response.responseObject?.statusText || 'OK';
-  };
-}
-export function getResponseExtension(index: number) {
-  return (state: HttpState[]) => {
-    const response = state[index].response.responseObject;
-
-    if (response && response.headers) {
-      const contentType = response.headers['content-type'];
-
-      if (contentType) {
-        if (contentType.includes('application/json')) {
-          return 'json';
-        } else if (contentType.includes('text/html')) {
-          return 'html';
-        }
-      }
+    if (!contentType) {
+      return '';
+    }
+    if (contentType.includes('application/json')) {
+      return 'json';
+    }
+    if (contentType.includes('text/html')) {
+      return 'html';
     }
 
     return '';
-  };
-}
-export function getResponseTime(index: number): (state: HttpState[]) => number {
-  return (state: HttpState[]) => {
-    return state[index].response.time;
   };
 }
